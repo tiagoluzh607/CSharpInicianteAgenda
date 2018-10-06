14269c4c912fc7fab90caa53f3ef8f7753517674
tiagoluz <tiagoluz.h607@gmail.com>
2018-10-06 00:41:49 -0300

    COde - Adicionando uma lista de strings ao projeto

--- AgendaPet/Program.cs
+++ AgendaPet/Program.cs
@@ -11,25 +11,23 @@ namespace AgendaPet
         static void Main(string[] args)
         {
 
-            string nome1;
-            string nome2;
-            string nome3;
+            List<string> listaDeNomes = new List<string>();
+            string nome;
+
 
             Console.WriteLine("### Olá meu nome é Sonserina, sua agenda para botar nomes ####\n");
 
             Console.WriteLine("Digite o Nome ");
-            nome1 = Console.ReadLine();
+            nome = Console.ReadLine();
+            listaDeNomes.Add(nome);
 
-            Console.WriteLine("Digite o Nome ");
-            nome2 = Console.ReadLine();
-
-            Console.WriteLine("Digite o Nome ");
-            nome3 = Console.ReadLine();
 
             Console.WriteLine("Lista de Nomes da Sonserina");
-            Console.WriteLine(nome1);
-            Console.WriteLine(nome2);
-            Console.WriteLine(nome3);
+
+            foreach (string item in listaDeNomes) {
+                Console.WriteLine(item);
+            }
+;
 
 
             Console.Read();
